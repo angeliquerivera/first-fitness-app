c8d795b34e75949a58300cdbce9c4bddff469f60
Angelique Rivera <angeliqueriveramorales@gmail.com>
2023-01-25 20:20:22 -0500

    add API to app

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
 const SearchExercises = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com",
+        exerciseOptions
+      );
+
+      console.log(exercisesData);
+    }
+  };
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" padding="20px">
       <Typography
@@ -21,8 +36,8 @@ const SearchExercises = () => {
             borderRadius: "40px",
           }}
           height="76px"
-          value=""
-          onChange={(e) => {}}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
@@ -39,6 +54,7 @@ const SearchExercises = () => {
           position: "absolute",
           right: "0",
         }}
+        onClick={handleSearch}
       >
         Search
       </Button>
