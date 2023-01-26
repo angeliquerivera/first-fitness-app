c6d3f7023da1cc2afc7c17d0ef8b5bc8334586a1
Angelique Rivera <angeliqueriveramorales@gmail.com>
2023-01-25 22:23:29 -0500

    feat: finished fetching data in SearchExercises.js

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -5,15 +5,35 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExercises = () => {
   const [search, setSearch] = useState("");
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesDate = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+      setBodyParts(["all"], ...bodyPartsData);
+    };
+  });
 
   const handleSearch = async () => {
     if (search) {
-      const exercisesData = await fetchData(
-        "https://exercisedb.p.rapidapi.com",
+      const exerciseData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
         exerciseOptions
       );
 
-      console.log(exercisesData);
+      const searchedExercises = exerciseData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+      );
+
+      setSearch("");
+      setExercises(searchedExercises);
     }
   };
 
