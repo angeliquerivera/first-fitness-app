7804c040fe0eabd4e0e8c3e9f89001cb9aee061a
Angelique Rivera <angeliqueriveramorales@gmail.com>
2023-01-30 22:18:26 -0500

    feat: add ExerciseVideos component and finished code at 2hrs12m40s

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -25,10 +25,11 @@ const ExerciseDetail = () => {
       setExerciseDetail(exerciseDetailData);
 
       const exerciseVideosData = await fetchData(
-        `${youtubeSearchUrl}/search?=q${exerciseDetailData.name}`,
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
         youtubeOptions
       );
-      setExerciseVideos(exerciseVideosData);
+
+      setExerciseVideos(exerciseVideosData.contents);
     };
 
     fetchExercisesData();
